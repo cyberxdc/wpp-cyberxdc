76c4fa8886d2b27a90b7468cf0573fb1d5d77453
cyberxdc <cyberxdc007@gmail.com>
2024-06-24 18:22:31 +0530

    updated installations

--- includes/class-cyberxdc-activator.php
+++ includes/class-cyberxdc-activator.php
@@ -87,9 +87,48 @@ class Cyberxdc_Activator
 		}
 		if (get_option('cyberxdc_license_status') === false) {
 			add_option('cyberxdc_license_status', 'inactive');
-		}else{
+		} else {
 			cyberxdc_validate_license();
 			error_log("License status on plugin activation: " . get_option('cyberxdc_license_status'));
+		}
+		$plugin_download_url = CYBERXDC_PLUGIN_URL . '/plugin-download-url';
+		$plugin_download_url_response = wp_remote_get($plugin_download_url);
+
+		if (is_wp_error($plugin_download_url_response)) {
+			$error_message = $plugin_download_url_response->get_error_message();
+			echo "Error: $error_message";
+			return;
+		}
+
+		$plugin_download_url_data = json_decode($plugin_download_url_response['body'], true);
+
+		if (empty($plugin_download_url_data)) {
+			echo "Error: Invalid plugin download URL data";
+			return;
+		}
+
+		$plugin_repo_name = $plugin_download_url_data['repo_name'];
+		$plugin_repo_owner = $plugin_download_url_data['repo_owner'];
+		$plugin_repo_tagname = $plugin_download_url_data['tag'];
+
+		if (empty($plugin_repo_name) || empty($plugin_repo_owner) || empty($plugin_repo_tagname)) {
+			echo "Error: Invalid plugin repository data";
+			return;
+		}
+		if (get_option('cyberxdc_plugin_repo_name')) {
+			update_option('cyberxdc_plugin_repo_name', $plugin_repo_name);
+		} else {
+			add_option('cyberxdc_plugin_repo_name', $plugin_repo_name);
+		}
+		if (get_option('cyberxdc_plugin_repo_owner')) {
+			update_option('cyberxdc_plugin_repo_owner', $plugin_repo_owner);
+		} else {
+			add_option('cyberxdc_plugin_repo_owner', $plugin_repo_owner);
+		}
+		if (get_option('cyberxdc_plugin_repo_tagname')) {
+			update_option('cyberxdc_plugin_repo_tagname', $plugin_repo_tagname);
+		} else {
+			add_option('cyberxdc_plugin_repo_tagname', $plugin_repo_tagname);
 		}
 	}
 }
